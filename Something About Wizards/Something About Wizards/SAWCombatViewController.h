04b50b832b7ebc1aa053094efe62fabf6bf09f06
Alexander Agosta <alexanderagosta@gmail.com>
2016-07-17 15:25:43 -0700

    Combat 101

---
+++ Something About Wizards/Something About Wizards/SAWCombatViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAWCombatViewController.h
+//  Something About Wizards
+//
+//  Created by Alexander.Agosta on 7/17/16.
+//  Copyright © 2016 Bozo Games. All rights reserved.
+//
+
+#import "SAWBaseViewController.h"
+
+@interface SAWCombatViewController : SAWBaseViewController
+
+@end
